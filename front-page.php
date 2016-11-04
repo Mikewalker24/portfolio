c745c1ec16bd6f0fb3c8c3a86b6b8bbd1e33a62d
Mike Walker <mikewalker24@gmail.com>
2016-11-04 10:27:42 -0400

    friday morning backup

--- front-page.php
+++ front-page.php
@@ -20,8 +20,60 @@
 		        <?php endwhile; ?>
 	        </div>
 
-			
-
+        <section class="featured-category">
+			<div class="container">
+				<div class="left">
+					<figure>
+						<img src="https://unsplash.it/700/800" alt="">
+						<figcaption>
+							<h2>Title</h2>
+							<p>Whatever whatever</p>
+						</figcaption>
+					</figure>
+				</div>
+				<div class="right">
+					<div class="top">
+						<div class="top-left">
+						<figure>
+							<img src="https://unsplash.it/700/800" alt="">
+							<figcaption>
+								<h2>Title</h2>
+								<p>Whatever whatever</p>
+							</figcaption>
+						</figure>
+							
+						</div>
+						<div class="top-right">
+							<figure>
+							<img src="https://unsplash.it/700/800" alt="">
+							<figcaption>
+								<h2>Title</h2>
+								<p>Whatever whatever</p>
+							</figcaption>
+						</figure>
+						</div>
+					</div>
+					<div class="bottom">
+						<figure>
+							<img src="https://unsplash.it/1400/800" alt="">
+							<figcaption>
+								<h2>Title</h2>
+								<p>Whatever whatever</p>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+			</div>
+        <section class="new-product">
+	        <div class="flickity" data-flickity='{ "wrapAround": true }'>
+	        	<?php while(has_sub_field('hero_slider')): ?>
+		        	<div class="flickity-cell" style="background-image: url(<?php the_sub_field('hero_image'); ?>);background-size: cover;">
+		        		<h2><?php the_sub_field('hero_title'); ?></h2>
+		        		<p><?php the_sub_field('hero_subtitle'); ?></p>
+		        		<div class="button-container"><a href="#">Shop Now</a></div>
+		        	</div>
+		        <?php endwhile; ?>
+	        </div>
         </section>
 
       <?php endwhile; // end the loop?>
